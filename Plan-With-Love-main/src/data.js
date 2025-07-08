d096b61e59130f2e87ee6d8d8322f122b4c13a95
Sujit_0210 <155601038+SujitYalmar@users.noreply.github.com>
2025-07-08 10:01:43 +0530

    fix: Correct duplicate information in Agra city description

--- Plan-With-Love-main/src/data.js
+++ Plan-With-Love-main/src/data.js
@@ -3,7 +3,7 @@ const data = [
     {
         id:1,
         name:"Agra",
-        info:"Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+        info:"Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
         images:"https://cdn.thecodehelp.in/Agra.jpeg",
         price:"34,758",
     },
